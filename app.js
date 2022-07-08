4f533b8549b2dceb289d09b3c73c0c2f0cffbdaf
Damian <damianrod@gmail.com>
2022-07-08 20:33:53 -0300

    funciona mercado pago, en el repo

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const static = require('./modules/static');
-const session = require('express-session');
+const express = require("express");
+const cors = require("cors");
+const static = require("./modules/static");
+const session = require("express-session");
 const app = express();
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3030;
 const listen = () => console.log(`Starting on http://localhost:${port}`);
-app.set('view engine', 'ejs');
-app.set('views', './views')
-app.listen(port,listen());
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.listen(port, listen());
 app.use(cors());
-app.use(static('../public'))
+app.use(static("../public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
+app.use(
+  session({
     resave: true,
     saveUninitialized: true,
-    secret: 'secret',
-}));
+    secret: "secret",
+  })
+);
 
 // Step 2
-// app.use(???)
+app.use(require("./middlewares/cart"));
 
-app.use(require('./routes/main.routes'))
-app.use('/checkout',require('./routes/checkout.routes'))
+app.use(require("./routes/main.routes"));
+app.use("/checkout", require("./routes/checkout.routes"));
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,8 +1,13 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
-const {index,addCart, updateCart, removeCart} = require('../controllers/main.controller');
-router.get('/',index)
-router.post('/cart/add',addCart)
-router.post('/cart/update',updateCart)
-router.post('/cart/add',removeCart)
-module.exports = router
+const {
+  index,
+  addCart,
+  updateCart,
+  removeCart,
+} = require("../controllers/main.controller");
+router.get("/", index);
+router.post("/cart/add", addCart);
+router.post("/cart/update", updateCart);
+router.post("/cart/delete", removeCart);
+module.exports = router;
